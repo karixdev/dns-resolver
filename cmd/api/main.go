0368c387386118e1f08893dcf093a8334b5f944b
karixdev <kari.klucz@gmail.com>
2024-06-17 21:54:47 +0200

    init

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,16 +1,4 @@
 package main
 
-import (
-	"dns-resolver/internal/server"
-	"fmt"
-)
-
 func main() {
-
-	server := server.NewServer()
-
-	err := server.ListenAndServe()
-	if err != nil {
-		panic(fmt.Sprintf("cannot start server: %s", err))
-	}
 }
